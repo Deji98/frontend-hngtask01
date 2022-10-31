96e7bc491ba9377e408767912c7024d27ac4823d
Dejiadesina <apple@macs-MacBook-Pro.local>
2022-10-31 08:41:13 +0100

    fixed redirects error

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -15,37 +15,37 @@ function Dashboard(){
   <div className='twitter'>
     <h3>Twitter Link:</h3>
     <div id='twitter' className='twitter-link'>
-    <a href='https://twitter.com/AdesinaAdedej20'>AdesinaAdedej20</a>
+    <a href=' https://twitter.com/AdesinaAdedej20' target="_blank">AdesinaAdedej20</a>
     </div>
   </div>
   <div className='Zuri'>
 <h3>Zuri Team</h3>
 <div id='btn__Zuri' className='zuri-link'>
-    <a href='https://training.zuri.team/'>Zuri Team</a>
+    <a href=' https://training.zuri.team/' target="_blank">Zuri Team</a>
     </div>
   </div>
   <div id='books' className='Books'>
 <h3>Zuri Books</h3>
 <div className='books-link'>
-    <a href='https://books.zuri.team/'>Zuri Books</a>
+    <a href=' https://books.zuri.team/' target="_blank">Zuri Books</a>
     </div>
   </div>
   <div id='book__python' className='python'>
 <h3>Python Books</h3>
 <div className='python-link'>
-    <a href='https://books.zuri.team/'>Python Books</a>
+    <a href=' https://books.zuri.team/' target="_blank">Python Books</a>
     </div>
   </div>
   <div id='pitch' className='background'>
 <h3>Background Check:</h3>
 <div className='background-link'>
-    <a href='https://background.zuri.team/'>Background Check for coders</a>
+    <a href=' https://background.zuri.team/' target="_blank">Background Check for coders</a>
     </div>
   </div>
   <div id='book__design' className='design'>
 <h3>Design Books</h3>
 <div className='design-link'>
-    <a href='https://books.zuri.team/design-rules'>Design Books</a>
+    <a href=' https://books.zuri.team/design-rules' target="_blank">Design Books</a>
     </div>
   </div>
   <div className='dev'>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "../App.css"
+import {AiOutlineGithub} from "react-icons/ai"
 
 function Home() {
   return (
     <div className='home'>
+    check source code here:
+    <a href=' https://github.com/Deji98/frontend-hngtask01' target="_blank"><AiOutlineGithub /></a>
+   
+  
     <h1>Welcome to the Landing Page, click the button below to go to your Dashboard</h1>
     <Link style={{textDecoration:"none", backgroundColor:"aqua", color:"darkblue", padding:"25px", paddingTop:"15px", paddingBottom:"15px", paddingLeft:"35px", paddingRight:"35px", borderRadius:"10px"}} to={'Dashboard'} >Dashboard</Link>
+  
+   
     </div>
   )
 }
